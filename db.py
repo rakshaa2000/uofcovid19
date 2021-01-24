250fc775be51b7f7e7fc73d78f126ec4d4cb5fed
TKH Student 2 <tkhstudent2@TKHs-Air-2.fios-router.home>
2021-01-24 05:22:23 -0500

    updating local

--- db.py
+++ db.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_pymongo import pymongo
 from app import app
 
-# CONNECTION_STRING = "mongodb+srv://Clariza_Mayo:Foreve6434!@cluster0.y9k04.mongodb.net/<dbname>?retryWrites=true&w=majority"
-# client = pymongo.MongoClient(CONNECTION_STRING)
-# db = client.get_database('flask_mongodb_atlas')
-# user_collection = pymongo.collection.Collection(db, 'user_collection')
-
-
-CONNECTION_STRING = "mongodb+srv://Judi_Desire:Foreve6434!@uofcc.g7ndo.mongodb.net/<dbname>?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://Clariza_Mayo:Foreve6434!@cluster0.y9k04.mongodb.net/<dbname>?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('flask_mongodb_atlas')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+
+# CONNECTION_STRING = "mongodb+srv://Judi_Desire:Foreve6434!@uofcc.g7ndo.mongodb.net/<dbname>?retryWrites=true&w=majority"
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('flask_mongodb_atlas')
+# user_collection = pymongo.collection.Collection(db, 'user_collection')
 
