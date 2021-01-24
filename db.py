6117feadf01739e5d473730de7348b99d9c90ee8
berryny <judesire@yahoo.com>
2021-01-23 20:34:20 -0500

    connect database

--- db.py
+++ db.py
@@ -6,3 +6,11 @@ CONNECTION_STRING = "mongodb+srv://Clariza_Mayo:Foreve6434!@cluster0.y9k04.mongo
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('flask_mongodb_atlas')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+
+
+# CONNECTION_STRING = "mongodb+srv://Judi_Desire:Foreve6434!@uofcc.g7ndo.mongodb.net/<dbname>?retryWrites=true&w=majority"
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('uofcc')
+# user_collection = pymongo.collection.Collection(db, 'uofcc_users')
+
